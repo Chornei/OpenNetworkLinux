aff64aeb2b6fbff046cdce0103154b6523f19d52
Steven Noble <steven.noble@bigswitch.com>
2016-07-11 21:13:48 -0700

    removing other references to r0

--- packages/platforms/alpha/x86-64/x86-64-alpha-networks-snx60a0-486f/platform-config/src/python/x86_64_alpha_networks_snx60a0_486f/__init__.py
+++ packages/platforms/alpha/x86-64/x86-64-alpha-networks-snx60a0-486f/platform-config/src/python/x86_64_alpha_networks_snx60a0_486f/__init__.py
@@ -1,9 +1,9 @@
 from onl.platform.base import *
 from onl.platform.celestica import *
 
-class OnlPlatform_x86_64_alpha_networks_snx60a0_486f_r0(OnlPlatformCelestica,
+class OnlPlatform_x86_64_alpha_networks_snx60a0_486f(OnlPlatformCelestica,
                                             OnlPlatformPortConfig_48x10_6x40):
-    PLATFORM='x86-64-alpha-networks-snx60a0-486f-r0'
+    PLATFORM='x86-64-alpha-networks-snx60a0-486f'
     MODEL="Alpha networks"
     SYS_OBJECT_ID=".2060.1"
 

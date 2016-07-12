dfb337f97ccae017770bde0f8e720b0a64861236
Steven Noble <steven.noble@bigswitch.com>
2016-07-12 04:05:16 +0000

    Initial addition of Alpha Networks SNX60A0 platform

---
+++ packages/platforms/alpha/vendor-config/src/python/alpha/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from onl.platform.base import *
+
+class OnlPlatformAlpha(OnlPlatformBase):
+    MANUFACTUTER='Alpha'
+    PRIVATE_ENTERPRISE_NUMBER=12290
+
---
+++ packages/platforms/alpha/x86-64/x86-64-alpha-networks-snx60a0-486f/platform-config/src/python/x86_64_alpha_networks_snx60a0_486f_r0/__init__.py
@@ -0,0 +1,9 @@
+from onl.platform.base import *
+from onl.platform.celestica import *
+
+class OnlPlatform_x86_64_alpha_networks_snx60a0_486f_r0(OnlPlatformCelestica,
+                                            OnlPlatformPortConfig_48x10_6x40):
+    PLATFORM='x86-64-alpha-networks-snx60a0-486f-r0'
+    MODEL="Alpha networks"
+    SYS_OBJECT_ID=".2060.1"
+

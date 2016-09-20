769276a2ec0c29086c8ba3bd3c25c4bd8635f965
Steven Noble <steven.noble@bigswitch.com>
2016-09-20 19:46:46 +0000

    updates table of contents to have a main page

---
+++ packages/base/any/onlp/src/onlp/module/toc.h
@@ -0,0 +1,31 @@
+/*! \mainpage Open Network Linux Platform Library Documentation
+ * \section intro_sec Introduction
+ *
+ * Open Network Linux
+ * ==================
+ *
+ * Check out the main/offical website at http://opennetlinux.org .
+ *
+ * Open Network Linux (ONL) is a Linux distribution for bare metal switches.  ONL
+ * builds an ONIE-compatible installer and a switch image which contains a complete
+ * Debian distribution with added drivers and configuration for running on bare metal
+ * switches.
+ *
+ * \section license_sec License
+ *
+ * Software License
+ * -----------------
+ *
+ * Licenses for the software are described under the [LICENSE](LICENSE) file.  Download or use of the software implies consent.
+ *
+ * \section document_sec Documentation
+ *
+ * Documentation
+ * -------------
+ *
+ * http://www.opennetlinux.org/docs/build
+ *
+ * Instructions for building ONL from scratch.
+ *
+ * Please see the docs directory for additional information.
+ */

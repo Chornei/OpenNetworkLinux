3bd7200726c41c1428b575442f7c4300f18be4c9
Steven Noble <steven.noble@bigswitch.com>
2016-09-26 15:03:40 -0700

    remove debugging add messages

--- tools/onlpgen.sh
+++ tools/onlpgen.sh
@@ -4,15 +4,15 @@ if [ $# -eq 0 ]
       echo "No arguments supplied"
       exit 1
 fi
-set -x
 VARIABLE=$1
 DASH=`echo $1 | tr '_' '-'`
 UNDER=`echo $1 | tr '-' '_'`
-echo $1
-cp -av $ONL/packages/platforms/onlp-stub/x86_64/platform_underscore/onlp .
+echo "building ONLP stub for platform" $1
+cp -a $ONL/packages/platforms/onlp-stub/x86_64/platform_underscore/onlp .
 grep -r -l platform_underscore onlp | xargs sed -i "s/platform_underscore/$UNDER/g"
 grep -r -l platform-dash onlp | xargs sed -i "s/platform-dash/$DASH/g"
 find ./onlp -type d -name "*platform_underscore*" | xargs rename "s/platform_underscore/$UNDER/g"
 find ./onlp -type d -name "*platform-dash*" | xargs rename "s/platform-dash/'$DASH'/g"
 find ./onlp -type f -name "*platform_underscore*" | xargs rename "s/platform_underscore/$UNDER/g"
 find ./onlp -type f -name "*platform-dash*" | xargs rename "s/platform-dash/'$DASH'/g"
+echo "The ONLP stub is ready, look for MODIFY in the files to see where to modify values"
